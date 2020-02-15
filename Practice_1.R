0b28039282d5f33be372103bf4e2c000c1202ac9
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-02-14 21:18:07 -0800

    Two and a half exercises
    Exercise 1, 2 and half of exercise 3 were done

--- Practice_1.R
+++ Practice_1.R
@@ -1,15 +1,30 @@
-#Test the Law Of Large Numbers for N random normally distributed numbers with mean = 0, stdev=1:
-  
-#Create an R script that will count how many of these numbers fall between -1 and 1 and divide
-# by the total quantity of N
+install.packages("gganimate")
+library(gganimate)
+install.packages("gifski")
+library(gifski)
+
+#Test the Law Of Large Numbers for N random normally distributed numbers 
+#with mean = 0, stdev=1:
+set.seed(124)
+
+#Create an R script that will count how many of these numbers fall between 
+#-1 and 1 and divide by the total quantity of N
 
 #You know that E(X) = 68.2%
 #Check that Mean(Xn)->E(X) as you rerun your script while increasing N
 
 #Hint:
 #1. Initialize sample size
+sample <- rnorm(100)
+
 #2. Initialize counter
+counter <- 0
+
 #3. loop for(i in rnorm(size))
+for(i in rnorm(100)){
+  print(i)
+}
+
 #4. Check if the iterated variable falls
 #5. Increase counter if the condition is true
 #6. return a result <- counter / N
