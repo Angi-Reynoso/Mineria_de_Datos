e0b0c1c79fdffefcce52fce14b9d51be9691bdd1
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-02-18 08:37:12 -0800

    Update Practice_2
    Exercises
    5-9
    16-20

--- Practice_2.R
+++ Practice_2.R
@@ -20,11 +20,29 @@ floor(2.9)
 trunc(5.9)
 trunc(5.1)
 
-#5. max( ):  
+#5. max( ):
+?max()
+vector = c(5,6,7,8,9)
+max(vector)
+
 #6. min( ):
-#7. unique( ):   
+?min()
+vector = c(5,6,7,8,9)
+min(vector)
+
+#7. unique( ):
+?unique()
+v <- c(5,10,0,0,2) 
+unique(v)
+
 #8. round( ):  
+?round()
+round(.5 + -2:4)
+
 #9. signif( ):
+?signif()
+x2 <- pi * 100^(-1:3)
+signif(x2, 3)
 
 #Character Functions  
 #10. substr(x, start, stop):  
@@ -56,7 +74,32 @@ sum(vector)
 
 #Other Functions 
 #16. all( ):  
+?all()
+range(x <- sort(round(stats::rnorm(10) - 1.2, 1)))
+if(all(x < 0)) cat("all x values are negative\n")
+all(logical(0))
+
 #17. any( ):  
+?any()
+range(x <- sort(round(stats::rnorm(10) - 1.2, 1)))
+if(any(x < 0)) cat("x contains negative values\n")
+
 #18. apropos( ):  
+?apropos()
+apropos("GLM")                      
+apropos("GLM", ignore.case = FALSE) 
+apropos("lq")
+
 #19. find( ):
+?find()
+cor <- 1:pi
+find("cor")                       
+find("cor", numeric = TRUE)                
+find("cor", numeric = TRUE, mode = "function")  
+rm(cor)
+
 #20. diff( ):
+?diff()
+x <- cumsum(cumsum(1:10))
+diff(x, lag = 2)
+diff(x, differences = 2)
