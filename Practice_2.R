aecf44115fef0a40c61a6d896b2226785e0b531f
Angélica Reynoso <52439492+Angi-Reynoso@users.noreply.github.com>
2020-02-17 18:50:55 -0800

    Create Practice_2.R

---
+++ Practice_2.R
@@ -0,0 +1,27 @@
+# Funtions in R
+
+### Numerical Functions  
+1. abs( ):  
+2. ceiling( ):  
+3. floor( ):  
+4. trunc( ):
+5. max( ):  
+6. min( ):
+7. unique( ):   
+8. round( ):  
+9. signif( ):
+
+### Character Functions  
+10. substr(x, start, stop):  
+11. toupper( ):  
+12. tolower( ):  
+13. mean( ):  
+14. median( ):  
+15. sum( ):  
+
+### Other Functions 
+16. all( ):  
+17. any( ):  
+18. apropos( ):  
+19. find( ):
+20. diff( ):
