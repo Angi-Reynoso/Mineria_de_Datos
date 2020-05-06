046f93e7ba6595c457b3396105e7af5888c1bd2d
Angélica Reynoso <52439492+Angi-Reynoso@users.noreply.github.com>
2020-05-05 18:02:28 -0700

    Update Practice_1.R
    Minor corrections

--- Practices/Practice_1/Practice_1.R
+++ Practices/Practice_1/Practice_1.R
@@ -8,18 +8,18 @@ dataset <- read.csv('Sea_Ice.csv')
 # Splitting the dataset into the Training set and Test set
 # Install.packages('caTools)
 library(caTools)
-set.seed(123) #aleatorios uniformes
-split <- sample.split(dataset$Extent, SplitRatio = 2/3) #24 datos de entrenamiento y 10 de prueba
-training_set <- subset(dataset, split == TRUE) #son 24 aleatorios
-test_set <- subset(dataset, split == FALSE) #son 13, con estos se prueba el modelo
+set.seed(123)
+split <- sample.split(dataset$Extent, SplitRatio = 2/3) 
+training_set <- subset(dataset, split == TRUE) 
+test_set <- subset(dataset, split == FALSE) 
 
 # Fitting Simple Linear Regression to the Training set
 regressor = lm(formula = Extent ~ Year,
-               data = dataset) #lm para informacion
-summary(regressor) #regressor es un objeto, segun los asteriscos es la correlacion, 2 o mas=mayor probabilidad de que funcione el modelo
+               data = dataset) 
+summary(regressor) 
 
 # Predicting the Test set results
-y_pred = predict(regressor, newdata = test_set) #enviar variables de prueba
+y_pred = predict(regressor, newdata = test_set) 
 
 # Visualising the Training set results
 library(ggplot2)
@@ -30,7 +30,7 @@ ggplot() +
             color = 'red') +
   ggtitle('Extent vs Year (Training Set)') +
   xlab('Year') +
-  ylab('Extent') #x,y=datos$columna 
+  ylab('Extent') 
 
 # Visualising the Test set results (datos reales)
 ggplot() +
