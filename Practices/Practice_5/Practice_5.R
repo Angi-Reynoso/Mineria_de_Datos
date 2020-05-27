898ac4ee56bf6da72e85b4162a498b66a61d8230
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-05-26 20:34:26 -0700

    Update Practice_5.R

--- Practices/Practice_5/Practice_5.R
+++ Practices/Practice_5/Practice_5.R
@@ -51,7 +51,6 @@ cm2
 cm3
 cm4
 
-
 library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
@@ -66,6 +65,38 @@ y_grid4 = predict(classifier4, newdata = grid_set)
 
 plot(set[, -3],
      main = 'SVM (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+
+contour(X1, X2, matrix(as.numeric(y_grid1), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid1 == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+contour(X1, X2, matrix(as.numeric(y_grid2), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid2 == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+contour(X1, X2, matrix(as.numeric(y_grid3), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid3 == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+contour(X1, X2, matrix(as.numeric(y_grid4), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid4 == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+
+y_grid1 = predict(classifier1, newdata = grid_set)
+y_grid2 = predict(classifier2, newdata = grid_set)
+y_grid3 = predict(classifier3, newdata = grid_set)
+y_grid4 = predict(classifier4, newdata = grid_set)
+
+plot(set[, -3], main = 'SVM (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 
