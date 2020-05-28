116967cb7ae91b3d700952e74a8d64a3756be371
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-05-28 09:01:39 -0700

    Update Exam.R

--- Exams/Exam.R
+++ Exams/Exam.R
@@ -18,3 +18,29 @@ df.movies = movies[(movies$Genre == "action"|movies$Genre == "adventure"
                     |movies$Studio == "Fox"|movies$Studio == "Paramount Pictures"
                     |movies$Studio == "Sony"|movies$Studio == "Universal"
                     |movies$Studio == "WB"),]
+windowsFonts("Comic Sans MS" = windowsFont("Comic Sans MS"))
+
+
+library(ggplot2)
+ggplot(df.movies, aes(x = Genre, y = GrossPercentageUS)) + 
+  geom_jitter(aes(color = Studio, size = Budget_Millions)) + 
+  geom_boxplot(alpha = 0.7) + 
+  xlab("Genre") + ylab("Gross % US") + 
+  ggtitle("Domestic Gross % by Genre") + 
+  theme(axis.title.x = element_text(color = "blue",
+                                    size = 17,
+                                    family = "Comic Sans MS"),
+        axis.title.y = element_text(color = "blue", 
+                                    size = 17,
+                                    family = "Comic Sans MS"),
+        axis.text.x = element_text(family = "Comic Sans MS", 
+                                   size = 12),
+        axis.text.y = element_text(family = "Comic Sans MS", 
+                                   size = 12),
+        legend.title = element_text(family = "Comic Sans MS", 
+                                    size = 14),
+        legend.text = element_text(family = "Comic Sans MS"),
+        plot.title = element_text(color = "black",
+                                  size = 23,
+                                  hjust = 0.5,
+                                  family = "Comic Sans MS"))
