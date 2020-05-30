428e26a1d750760d5cb7b6d6e73c143860f003fe
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-05-29 18:10:33 -0700

    Create Practice_7.R

---
+++ Practices/Practice_7/Practice_7.R
@@ -0,0 +1,78 @@
+getwd()
+setwd("C:/Users/GitHub/DataMining/MachineLearning/DesicionThree")
+getwd()
+
+
+dataset = read.csv('data_banknote_authentication.csv')
+head(dataset)
+colnames(dataset) <- c("Variance", "Skewness", "Kurtosis", "Entropy", 
+                       "Class")
+head(dataset)
+
+columns <- c('Variance', 'Entropy', 'Class')
+dataset = dataset[columns]
+
+
+dataset$Class = factor(dataset$Class, levels = c(0, 1))
+
+
+library(caTools)
+set.seed(123) 
+split = sample.split(dataset$Class, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+
+install.packages('rpart')
+library(rpart)
+classifier = rpart(formula = Class ~ .,
+                   data = training_set)
+
+
+y_pred = predict(classifier, newdata = test_set[-3], type = 'class')
+y_pred
+
+
+cm = table(test_set[, 3], y_pred)
+cm
+
+
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Variance', 'Entropy')
+y_grid = predict(classifier, newdata = grid_set, type = 'class')
+plot(set[, -3],
+     main = 'Decision Tree Classification (Training set)',
+     xlab = 'Variance of Wavelet Transformed image', 
+     ylab = 'Entropy of image',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Variance', 'Entropy')
+y_grid = predict(classifier, newdata = grid_set, type = 'class')
+plot(set[, -3], main = 'Decision Tree Classification (Test set)',
+     xlab = 'Variance of Wavelet Transformed image', 
+     ylab = 'Entropy of image',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+
+plot(classifier)
+text(classifier, cex=0.6)
